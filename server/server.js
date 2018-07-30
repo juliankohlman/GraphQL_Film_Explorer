f9cc7d361cd1db5ca3bc3029e5b157de00591136
Julian Kohlman <juliankohlman@gmail.com>
2018-07-30 11:27:32 -0400

    server setup w/nodemon

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+dotenv.config();
+const app = express();
+
+app.use(
+	'/graphql',
+	expressGraphQL({
+		schema,
+		graphiql: true
+	})
+);
+
+app.listen(4000, () => {
+	console.log('Listening...');
+});
