dd948b8ade5d9bbfe5ea63d3ce4348a02dfec0c1
Julian Kohlman <juliankohlman@gmail.com>
2018-07-31 19:30:17 -0400

    Working on search query for single movie by title

--- schema/schema.js
+++ schema/schema.js
@@ -6,27 +6,92 @@ const {
 	GraphQLString,
 	GraphQLInt,
 	GraphQLSchema,
-	GraphQLList
+	GraphQLList,
+	GraphQLID
 } = graphql;
 
 // now_playing
 const NewMoviesType = new GraphQLObjectType({
 	name: 'NewMovies',
 	fields: {
-		id: { type: GraphQLInt },
+		id: { type: GraphQLID },
 		poster_path: { type: GraphQLString },
 		title: { type: GraphQLString }
+	}
+});
+
+// trailers
+const VideoType = new GraphQLObjectType({
+	name: 'Video',
+	fields: {
+		id: { type: GraphQLID },
+		key: { type: GraphQLString }
+	}
+});
+
+// movie search
+const MovieSearchType = new GraphQLObjectType({
+	name: 'SearchMovie',
+	fields: {
+		query: { type: GraphQLString },
+		year: { type: GraphQLInt }
 	}
 });
 
 // single movie
 const MovieInfoType = new GraphQLObjectType({
 	name: 'MovieInfo',
-	fields: {}
+	fields: {
+		id: { type: GraphQLID },
+		overview: { type: GraphQLString },
+		title: { type: GraphQLString },
+		poster_path: { type: GraphQLString },
+		genres: { type: GraphQLString },
+		release_date: { type: GraphQLString },
+		vote_average: { type: GraphQLString },
+		production_companies: { type: GraphQLString },
+		runtime: { type: GraphQLString },
+		videos: {
+			type: new GraphQLList(VideoType),
+			args: { id: { type: GraphQLString } },
+			resolve(parentVal, args) {
+				return axios
+					.get(
+						`https://api.themoviedb.org/3/movie/${
+							parentValue.id
+						}/videos?api_key=${process.env.API}&language=en-US`
+					)
+					.then(res => res.data.results);
+			}
+		}
+	}
 });
+
 const RootQuery = new GraphQLObjectType({
 	name: 'RootQueryType',
 	fields: {
+		movieInfo: {
+			type: MovieInfoType,
+			args: { id: { type: GraphQLID } },
+			resolve(parentVal, args) {
+				return axios
+					.get(
+						`https://api.themoviedb.org/3/movie/${args.id}?api_key=${
+							process.env.API
+						}&language=en-US&page=1`
+					)
+					.then(res => {
+						const movie = res.data;
+						movie.genres = movie.genres.map(g => g.name).join(', ');
+						movie.production_companies = movie.production_companies
+							.map(c => c.name)
+							.join(', ');
+						// could leave the time interpretation for the front-end
+						movie.runtime += ' minutes.';
+						return movie;
+					});
+			}
+		},
 		newMovies: {
 			type: new GraphQLList(NewMoviesType),
 			resolve() {
