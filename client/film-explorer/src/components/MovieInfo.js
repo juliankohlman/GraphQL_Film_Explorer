6f16f5000ba625120aea58f2a7c5e13fcccfdfab
Julian Kohlman <juliankohlman@gmail.com>
2018-08-13 11:39:17 -0400

    debugging apollo-client issue.

--- client/film-explorer/src/components/MovieInfo.js
+++ client/film-explorer/src/components/MovieInfo.js
@@ -1,9 +1,169 @@
+// import React, { Component } from 'react';
+// import gql from 'graphql-tag';
+// import { Query, graphql } from 'react-apollo';
+// import { getMovieInfo } from '../queries/queries';
+// import MovieReviews from './MovieReviews';
+// import MovieCredits from './MovieCredits';
+
+// class MovieInfo extends Component {
+// 	constructor() {
+// 		super();
+// 		this.state = {
+// 			video: null
+// 		};
+// 	}
+
+// 	renderVideos(videos) {
+// 		return videos.map(video => {
+// 			return (
+// 				<img
+// 					key={video.id}
+// 					onClick={() => this.videoDisplay(video.key)}
+// 					className="video_thumbs"
+// 					alt="thumbnail"
+// 					src={`http://img.youtube.com/vi/${video.key}/0.jpg`}
+// 				/>
+// 			);
+// 		});
+// 	}
+
+// 	videoDisplay(video) {
+// 		this.setState({
+// 			video
+// 		});
+// 	}
+
+// 	videoToggle() {
+// 		if (this.state.video)
+// 			return (
+// 				<div className="youtube-video">
+// 					<p onClick={() => this.videoExit()}>close video</p>
+// 					<iframe
+// 						title={`this.state.video.name`}
+// 						width="560"
+// 						height="315"
+// 						src={`//www.youtube.com/embed/${this.state.video}`}
+// 						frameBorder="0"
+// 						allowFullScreen
+// 					/>
+// 				</div>
+// 			);
+// 	}
+
+// 	videoExit() {
+// 		this.setState({
+// 			video: null
+// 		});
+// 	}
+
+// 	render() {
+// 		console.log(this.state);
+
+// 		const id = this.props.match.params.id;
+// 		return (
+// 			<Query query={query} variables={{ id }}>
+// 				{({ loading, err, data }) => {
+// 					if (loading) return <div>loading</div>;
+// 					return (
+// 						<div>
+// 							<header
+// 								style={{
+// 									backgroundImage:
+// 										'url("https://image.tmdb.org/t/p/w500///' +
+// 										data.movieInfo.poster_path +
+// 										'")'
+// 								}}
+// 							>
+// 								<h2 className="title">{data.movieInfo.title}</h2>
+// 							</header>
+// 							<article className="wrapper">
+// 								<p className="description">{data.movieInfo.overview}</p>
+// 								<div className="sidebar">
+// 									<img
+// 										src={
+// 											'https://image.tmdb.org/t/p/w500///' +
+// 											data.movieInfo.poster_path
+// 										}
+// 										className="cover_image"
+// 										alt=""
+// 									/>
+// 									<ul>
+// 										<li>
+// 											<strong>Genre:</strong> {data.movieInfo.genres}
+// 										</li>
+// 										<li>
+// 											<strong>Released:</strong>
+// 											{data.movieInfo.release_date}
+// 										</li>
+// 										<li>
+// 											<strong>Rated:</strong> {data.movieInfo.vote_average}
+// 										</li>
+// 										<li>
+// 											<strong>Runtime:</strong> {data.movieInfo.runtime}
+// 										</li>
+// 										<li>
+// 											<strong>Production Companies:</strong>{' '}
+// 											{data.movieInfo.production_companies}
+// 										</li>
+// 									</ul>
+// 									<div className="videos">
+// 										{this.videoToggle()}
+// 										<h3>Videos</h3>
+// 										{/* videos */}
+// 										{this.renderVideos(data.movieInfo.videos)}
+// 									</div>
+// 									{/* reviews */}
+// 									<MovieReviews reviews={data.movieInfo.movieReviews} />
+// 								</div>
+// 								{/* credits */}
+// 								<MovieCredits credits={data.movieInfo.movieCredits} />
+// 							</article>
+// 						</div>
+// 					);
+// 				}}
+// 			</Query>
+// 		);
+// 	}
+// }
+
+// const query = gql`
+// 	query MovieInfo($id: String) {
+// 		movieInfo(id: $id) {
+// 			title
+// 			overview
+// 			poster_path
+// 			genres
+// 			release_date
+// 			vote_average
+// 			runtime
+// 			production_companies
+// 			videos {
+// 				id
+// 				key
+// 			}
+// 			movieReviews {
+// 				id
+// 				content
+// 				author
+// 			}
+// 			movieCredits {
+// 				id
+// 				character
+// 				name
+// 				profile_path
+// 				order
+// 			}
+// 		}
+// 	}
+// `;
+
+// export default graphql(query)(MovieInfo);
+
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
 import { Query, graphql } from 'react-apollo';
-import { getMovieInfo } from '../queries/queries';
 import MovieReviews from './MovieReviews';
 import MovieCredits from './MovieCredits';
-
 class MovieInfo extends Component {
 	constructor() {
 		super();
@@ -11,36 +171,6 @@ class MovieInfo extends Component {
 			video: null
 		};
 	}
-
-	videoDisplay(video) {
-		this.setState({
-			video
-		});
-	}
-
-	videoExit() {
-		this.setState({
-			video: null
-		});
-	}
-
-	videoToggle() {
-		if (this.state.video)
-			return (
-				<div className="youtube-video">
-					<p onClick={() => this.videoExit()}>close video</p>
-					<iframe
-						title={`this.state.video.name`}
-						width="560"
-						height="315"
-						src={`//www.youtube.com/embed/${this.state.video}`}
-						frameBorder="0"
-						allowFullScreen
-					/>
-				</div>
-			);
-	}
-
 	renderVideos(videos) {
 		return videos.map(video => {
 			return (
@@ -48,21 +178,49 @@ class MovieInfo extends Component {
 					key={video.id}
 					onClick={() => this.videoDisplay(video.key)}
 					className="video_thumbs"
-					alt="thumbnail"
 					src={`http://img.youtube.com/vi/${video.key}/0.jpg`}
+					alt=""
 				/>
 			);
+		});
+	}
+	videoDisplay(video) {
+		this.setState({
+			video
+		});
+	}
+	videoToggle() {
+		if (this.state.video)
+			return (
+				<div className="youtube-video">
+					<p onClick={() => this.videoExit()}>close</p>
+					<iframe
+						width="560"
+						height="315"
+						src={`//www.youtube.com/embed/${this.state.video}`}
+						frameborder="0"
+						allowfullscreen
+					/>
+				</div>
+			);
+	}
+	videoExit() {
+		this.setState({
+			video: null
 		});
 	}
 
 	render() {
 		const id = this.props.match.params.id;
 		return (
-			<Query>
+			<Query query={query} variables={{ id }}>
 				{({ loading, err, data }) => {
 					if (loading) return <div>loading</div>;
 					return (
 						<div>
+							<a id="home_btn" href="/">
+								Home
+							</a>
 							<header
 								style={{
 									backgroundImage:
@@ -122,5 +280,35 @@ class MovieInfo extends Component {
 		);
 	}
 }
+const query = gql`
+	query MovieInfo($id: String) {
+		movieInfo(id: $id) {
+			title
+			overview
+			poster_path
+			genres
+			release_date
+			vote_average
+			runtime
+			production_companies
+			videos {
+				id
+				key
+			}
+			movieReviews {
+				id
+				content
+				author
+			}
+			movieCredits {
+				id
+				character
+				name
+				profile_path
+				order
+			}
+		}
+	}
+`;
 
-export default graphql(getMovieInfo)(MovieInfo);
+export default graphql(query)(MovieInfo);
--- client/film-explorer/src/index.js
+++ client/film-explorer/src/index.js
@@ -2,20 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
 import { HashRouter, Switch, Route } from 'react-router-dom';
 import NewMovies from './components/NewMovies';
 import MovieInfo from './components/MovieInfo';
 
 import './styles/style.css';
 
-const client = new ApolloClient();
+// const client = new ApolloClient();
+
+const cache = new InMemoryCache();
+
+const client = new ApolloClient({
+	link: new HttpLink(),
+	cache
+});
 
 const Root = () => {
 	return (
 		<HashRouter>
 			<ApolloProvider client={client}>
 				<Switch>
-					<Route exact path="/graphql" component={NewMovies} />
+					<Route exact path="/" component={NewMovies} />
 					<Route exact path="/info/:id" component={MovieInfo} />
 				</Switch>
 			</ApolloProvider>
