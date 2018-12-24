ab22eb4f1c1c9ffad3f75219fac93409bbc71132
Julian Kohlman <juliankohlman@gmail.com>
2018-12-23 23:27:36 -0500

    Import logo into landing page component

--- client/film-explorer/src/components/LandingPage.js
+++ client/film-explorer/src/components/LandingPage.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
-
+import logo from '../style/assets/logo.png';
 const Wrapper = styled.div`
+	display: flex;
+	flex-flow: column;
 	margin: auto;
 	background: #2953a0;
+`;
+const Image = styled.img`
+	width: 175px;
+	height: 175px;
 `;
 
 const customLandingLinks = () => (
 	<Wrapper>
+		<Image src={logo} />
 		<div className="title">
 			<h1 className="app_title">The Film Explorer</h1>
 		</div>
