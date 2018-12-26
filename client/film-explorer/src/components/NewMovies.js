73fc3c9ed013e511c80660866f8b93fd07180adf
Julian Kohlman <juliankohlman@gmail.com>
2018-12-25 19:25:41 -0500

    Adjust background color of newMovies component

--- client/film-explorer/src/components/NewMovies.js
+++ client/film-explorer/src/components/NewMovies.js
@@ -6,9 +6,21 @@ import { getNewMovies } from '../queries/queries';
 
 const Wrapper = styled.div`
 	background: #083e4e;
+	background-size: cover;
 	height: 100vh;
 	display: flex;
 	flex-flow: column;
+`;
+
+const StyledLink = styled(Link)`
+	font-size: 35px;
+	font-family: Londrina Solid;
+	text-decoration: none;
+	color: white;
+	padding: 7%;
+	&:hover {
+		color: #d5441d;
+	}
 `;
 
 class NewMovies extends Component {
@@ -20,31 +32,39 @@ class NewMovies extends Component {
 		// * Render cards using material ui card components
 		return this.props.data.newMovies.map(movie => {
 			return (
-				<article key={movie.id} className="movie_list">
-					<Link to={'/info/' + movie.id}>
-						<img className="poster_img" src={movie.poster_path} alt="poster" />
-					</Link>
-				</article>
+				<Wrapper>
+					<article key={movie.id} className="movie_list">
+						<Link to={'/info/' + movie.id}>
+							<img
+								className="poster_img"
+								src={movie.poster_path}
+								alt="poster"
+							/>
+						</Link>
+					</article>
+				</Wrapper>
 			);
 		});
 	}
 	render() {
-		<Wrapper>
-			if (this.props.data.loading) return (
-			<div className="loading_message">
-				<h1>loading movies...</h1>
-			</div>
-			); return (
+		if (this.props.data.loading)
+			return (
+				<Wrapper>
+					<div className="loading_message">
+						<h1>loading movies...</h1>
+					</div>
+				</Wrapper>
+			);
+		return (
 			<div id="new_movies_container">
 				<div className="home_nav">
 					<a id="home_btn" href="/">
 						Home
 					</a>
+					{this.Movies()}
 				</div>
-				{this.Movies()}
 			</div>
-			);
-		</Wrapper>;
+		);
 	}
 }
 
