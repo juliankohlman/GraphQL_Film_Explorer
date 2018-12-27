659f88a1e0671e80b9039a49dcee8df09066c903
Julian Kohlman <juliankohlman@gmail.com>
2018-12-26 21:07:12 -0500

    Debugging heroku server issue

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
+const path = require('path');
 const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
 const cors = require('cors');
-
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema.js');
-dotenv.config();
-const app = express();
-const port_number = process.env.PORT || 4000;
 
-if (process.env.NODE === 'production') {
-	app.use(express.static('client/build'));
-}
+const publicPath = path.join(__dirname, '..', 'public');
+const port_number = process.env.PORT || 3000;
+dotenv.config();
+
+app.use(express.static(publicPath));
+
 app.get('*', (req, res) => {
-	res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	res.sendfile(path.join(publicPath, 'index.html'));
 });
 
 app.use(cors());
