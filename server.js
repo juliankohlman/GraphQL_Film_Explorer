7e0ed8739d2288bd06bbdfbaceed5ebdd3047535
Julian Kohlman <juliankohlman@gmail.com>
2018-12-26 20:46:25 -0500

    Configure express server for heroku deployment

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema.js');
 dotenv.config();
 const app = express();
-const port_number = app.listen(process.env.PORT || 4000);
+const port_number = process.env.PORT || 4000;
+
+if (process.env.NODE === 'production') {
+	app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+	res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.use(cors());
 
