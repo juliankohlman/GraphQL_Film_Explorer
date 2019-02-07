555ceae1337323d6f1ca69cf77604289789999e8
Julian Kohlman <juliankohlman@gmail.com>
2019-02-06 19:28:32 -0500

    Updates file structure

---
+++ film-explorer-client/src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../styles/assets/logo.png';
+// import style from '../styles/index.css';
+
+export default function Landing() {
+	return (
+		<div className="green">
+			<div className="">
+				{/* <img src={logo} alt="film explorer" /> */}
+				<p className="dtc v-mid tc bg-red">FUTURE LANDING PAGE</p>
+			</div>
+		</div>
+	);
+}
